c5e2fb21f8d766f240169061ecb3fb349ce6fdbd
kwakjinseop <xmcdk@naver.com>
2021-05-06 19:48:18 +0900

    5.6

--- Jinseop/Social_login/sociallogin/Googlelogin/models.py
+++ Jinseop/Social_login/sociallogin/Googlelogin/models.py
@@ -10,3 +10,4 @@ class Google(models.Model):
 
     def __str__(self):
         return self.title
+
--- Jinseop/Social_login/sociallogin/sociallogin/urls.py
+++ Jinseop/Social_login/sociallogin/sociallogin/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 import Googlelogin.views
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Googlelogin.views.home, name="home"),
     path('accounts/', include('allauth.urls')),
-
 ]
